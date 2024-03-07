dbc7f28f4cdb9f84bdbb3eeee0ba31840edb74ed
Martín Capuano <capuano.martin@outlook.com>
2024-03-07 18:35:26 -0300

    Empezando a lograr algo

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,6 @@
 const navbar = document.getElementById("navbar");
+const logo1 = document.getElementById("nav_logo1");
+const logo2 = document.getElementById("nav_logo2");
 console.log(navbar)
 let scrolled = false;
 window.onscroll = () => {
@@ -12,8 +14,16 @@ window.onscroll = () => {
     if (last_scrolled!=scrolled){
         if (!navbar.classList.contains("nav_hidden")&&scrolled){
             navbar.classList.add("nav_hidden");
+            logo1.classList.add("logo1_hidden");
+            logo2.classList.remove("logo2_hidden");
+            logo1.style = "display: none";
+            logo2.style = "display: flex";
         }else{
             navbar.classList.remove("nav_hidden")
+            logo1.classList.remove("logo1_hidden")
+            logo2.classList.add("logo2_hidden")
+            logo1.style = "display: flex";
+            logo2.style = "display: none";
         }
     }
     
