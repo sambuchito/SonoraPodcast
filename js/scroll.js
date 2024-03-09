50d4af96d06c336bf4bc1f476c42577f1a7ded15
Martín Capuano <capuano.martin@outlook.com>
2024-03-09 10:38:15 -0300

    Actualizando

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,13 @@
 const navbar = document.getElementById("navbar");
 const logo1 = document.getElementById("nav_logo1");
 const logo2 = document.getElementById("nav_logo2");
-console.log(navbar)
+const about = document.getElementById("about");
+
 let scrolled = false;
 window.onscroll = () => {
     let last_scrolled = false;
     last_scrolled = scrolled;
-    if (window.scrollY>80){
+    if (window.scrollY>380){
         scrolled = true;
     }else{
         scrolled = false;
@@ -16,10 +17,12 @@ window.onscroll = () => {
             navbar.classList.add("nav_hidden");
             logo1.classList.add("logo_hidden");
             logo2.classList.remove("logo_hidden");
+            about.classList.add("about_nav_hidden");
         }else{
             navbar.classList.remove("nav_hidden")
             logo1.classList.remove("logo_hidden")
             logo2.classList.add("logo_hidden")
+            about.classList.remove("about_nav_hidden");
         }
     }
     
