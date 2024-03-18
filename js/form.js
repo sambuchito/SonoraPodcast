ab21a9dbef9ad8b9083ea54d843e1de597236daa
sambuchito <139599455+sambuchito@users.noreply.github.com>
2024-03-18 00:47:43 -0300

    sidebar form

--- js/form.js
+++ js/form.js
@@ -1,3 +1,10 @@
 document.getElementById('submit').addEventListener('click', (event) => {
     event.preventDefault();
 });
+
+$(document).ready(function(){
+    $('.toggle').click(function(){
+      $('.sidebar-contact').toggleClass('active')
+      $('.toggle').toggleClass('active')
+    })
+  })
